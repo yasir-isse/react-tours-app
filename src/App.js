e16b89f906666b985c46da63e2a0c1f3080b9121
Yasir <code.yasir@gmail.com>
2022-04-09 02:32:44 +0300

    add Tours & Tour components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Loading from "./Loading";
+import Tours from "./Tours";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
   return (
     <main>
-      <Loading />
+      <Tours />
     </main>
   );
 }
---
+++ src/Tours.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Tours() {
+  return (
+    <div>
+      <h1>Tours</h1>
+    </div>
+  );
+}
+
+export default Tours;
