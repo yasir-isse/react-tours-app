3ab2bae1ce5abd56e66f39ce6f1449a68b01a776
Yasir <code.yasir@gmail.com>
2022-04-09 03:27:53 +0300

    add Tour component logic

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Tour() {
-  return <div>Tour</div>;
+function Tour({ id, image, name, price, info, handleRemove }) {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <section>
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">{price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show Less" : "Read More"}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => handleRemove(id)}>
+          Not Interested
+        </button>
+      </footer>
+    </section>
+  );
 }
 
 export default Tour;
